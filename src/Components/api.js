1ea8a70c783793bb3d037d1b0d6b49ca45a1da9f
mkp_company <mkp0131@naver.com>
2021-02-03 12:40:42 +0900

    3

--- src/Components/api.js
+++ src/Components/api.js
@@ -12,12 +12,42 @@ export const tvApi = {
 	airingToday: () => api.get('tv/airing_today'),
 	popular: () => api.get('tv/popular'),
 	topRated: () => api.get('tv/top_rated'),
-	detail: (tv_id) => api.get(`tv/${tv_id}`),
+	detail: (tv_id) => api.get(
+		`tv/${tv_id}`,
+		{
+			params: {
+				append_to_response: "videos"
+			}
+		}
+	),
+	search: (term) => api.get(
+		'search/tv', 
+		{
+			params: {
+				query: encodeURIComponent(term),
+			}
+		}
+	),
 }
 
 export const moviesApi = {
 	nowPlaying: () => api.get('movie/now_playing'),
 	upComing: () => api.get('movie/upcoming'),
 	popular: () => api.get('movie/popular'),
-	detail: (movie_id) => api.get(`movie/${movie_id}`),
+	detail: (movie_id) => api.get(
+		`movie/${movie_id}`,
+		{
+			params: {
+				append_to_response: "videos"
+			}
+		}
+	),
+	search: (term) => api.get(
+		'search/movie', 
+		{
+			params: {
+				query: encodeURIComponent(term),
+			}
+		}
+	),
 }
